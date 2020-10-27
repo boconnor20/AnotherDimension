2deb33123dc1f875ef944aaa5347f45d14d9021b
Brandon O'Connor <boconnor20@stuy.edu>
2020-10-27 01:26:03 -0400

    I tested 4e and it works for the provided test case!

--- ArrayOps.java
+++ ArrayOps.java
@@ -38,4 +38,13 @@ public class ArrayOps{
     return arr;
   }
   //4e
+  public static int sum(int[][] arr){
+    int sum = 0;
+    for(int i = 0; i < arr.length; i ++){
+      for(int j =0; j < arr[i].length; j ++){
+        sum += arr[i][j];
+      }
+    }
+    return sum;
+  }
 }
