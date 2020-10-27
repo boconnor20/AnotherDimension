0d867d3020bb6b39cbb68498a01fe588073e8b09
Brandon O'Connor <boconnor20@stuy.edu>
2020-10-27 01:23:37 -0400

    updated test cases to match the provided ones

--- ArrayOps.java
+++ ArrayOps.java
@@ -37,4 +37,5 @@ public class ArrayOps{
     }
     return arr;
   }
+  //4e
 }
--- Tester.java
+++ Tester.java
@@ -1,7 +1,16 @@
+import java.util.Arrays;
 public class Tester{
   public static void main(String[] args) {
-    int[][]x={{3,4},{3,4}};
-    System.out.println(ArrayOps.sum(ArrayOps.sumRows(x)));
+    int[][]  A = { {  1,  0, 12, -1 },
+                  {  7, -2,  2,  1 },
+                  { -5, -2,  2, -9 }
+               };
+    int[]  B  =   {  1, 3, 5 };
+    System.out.println(Arrays.toString(ArrayOps.sumRows(A))) ;
+    System.out.println(Arrays.toString(ArrayOps.largestInRows(A))) ;
+    System.out.println(ArrayOps.sum(B)) ;
+
+
   }
 
 }
